4c9ef84b9794582f6ad67a2a4ab2226b6bc41589
tkilcoy1 <tkilcoy1@binghamton.edu>
2023-12-11 11:57:06 -0500

    updated thrift file

--- fhe.thrift
+++ fhe.thrift
@@ -1,17 +1,17 @@
 # Thrift file describing basic operations for server and client
+namespace py fhe
 
 struct File {
   1: string path,
   2: binary data,
   3: binary ctx,
-  4. binary pub_key,
+  4: binary pub_key,
   5: binary relin_key,
   6: binary rotate_key
 }
   
 
 service FHE_Fileserver {
-  i32 initialize[
   i32 upload_file(1: File f),
   File download_file(1: string path),
   i32 add_file(1: string path, 2: binary data)
